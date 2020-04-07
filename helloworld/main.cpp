66145baa8ed76ae6a533f966fc0f076ee0a4c453
Alfred Chillitupa Quispihuanca <alfred.chillitupa@ucsp.edu.pe>
2020-04-07 08:04:19 -0500

    recibe datos

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int x,y,a,b,c;
+    cout << "Ingrese un valor para x" << endl;
+    cin >> x;
+    cout << "Ingresa un valor para y" << endl;
+    cin >> y;
+    a=x+y;
+    b=x-y;
+    c=x*y;
+    cout << "Muy bien!!" << endl;
+    cout << "La suma es: " << a << endl;
+    cout << "La resta es: " << b << endl;
+    cout << "La multiplicacion es: " << c << endl;
     return 0;
 }
