94c3f3683ebdc5d9ff7b5e7fe8885c5a02504d55
Alfred Chillitupa Quispihuanca <alfred.chillitupa@ucsp.edu.pe>
2020-04-08 08:52:41 -0500

    programa con variables legibles

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -4,17 +4,17 @@ using namespace std;
 
 int main()
 {
-    int x,y,a,b,c;
+    int x,y,suma,resta,multi;
     cout << "Ingrese un valor para x" << endl;
     cin >> x;
     cout << "Ingresa un valor para y" << endl;
     cin >> y;
-    a=x+y;
-    b=x-y;
-    c=x*y;
+    suma=x+y;
+    resta=x-y;
+    multi=x*y;
     cout << "Muy bien!!" << endl;
-    cout << "La suma de los numeros es: " << a << endl;
-    cout << "La resta de los numeros es: " << b << endl;
-    cout << "La multiplicacion los numeros es: " << c << endl;
+    cout << "La suma de los numeros es: " << suma << endl;
+    cout << "La resta de los numeros es: " << resta << endl;
+    cout << "La multiplicacion los numeros es: " << multi << endl;
     return 0;
 }
